4d03d723a39d38572c0d65f3f847846d68239aea
Sanatani-Dhruv <dhruvdds125@gmail.com>
2025-10-18 16:53:04 +0530

    (Process-status: almost-done) The image now uploads to currect directory but didn't check if it's totally bug free if not upload image

--- sql.sql
+++ sql.sql
@@ -7,7 +7,7 @@ create table products (
 	product_shortdesc varchar(100) not null,
 	product_longdesc varchar(500) not null,
 	product_stock varchar(11) not null,
-	product_imagepath varchar(100) not null
+	product_imagepath varchar(100) 
 );
 desc products;
 
