88eb3ae7e2bcd31c741142fac6275accc99b619e
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-01-09 16:40:25 +0530

    Basic index view file created

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0"/>
+    <title>{{ 'Home | ' . config('app.name') }}</title>
+</head>
+<body>
+    
+</body>
+</html>
